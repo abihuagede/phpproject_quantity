3a51c3dfa89cd6c6c21c45a616f20c92a18f2300
Abihu Kwashie Agede <abihuagede12@gmail.com>
2025-03-10 01:07:08 -1200

    Add files via upload

--- home.php
+++ home.php
@@ -1,5 +1,6 @@
 <?php
-session_start();
+
+session_start()
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,19 +12,7 @@ session_start();
 </head>
 
 <body>
-    this is the home page <br>
-    <form action="home.php" method="post">
-        <input type="submit" name="logout" value="logout">
-    </form>
+    home
 </body>
 
-</html><?php
-
-        echo $_SESSION['username'] . "<br>";
-        echo $_SESSION['password'] . "<br>";
-
-        if (isset($_POST['logout'])) {
-            session_destroy();
-            header("location:login.php");
-        }
-        ?>
+</html>
--- login.php
+++ login.php
@@ -1,36 +1,44 @@
-<?php
-session_start();
-?>
-<!DOCTYPE html>
-<html lang="en">
+ <!DOCTYPE html>
+ <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+ </head>
 
-<body>
-    <form action="login.php" method="post">
-        <label>UserName:</label><br>
-        <input type="text" name="username" placeholder="username"><br>
-        <label>UserName:</label><br>
-        <input type="password" name="password" placeholder="password"><br>
-        <input type="submit" name="login" value="login">
-    </form>
-</body>
+ <body>
+     <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])  ?>" method="post">
+         <input type="text" name="name" placeholder="Enter your name">
+         <input type="text" name="email" placeholder="Enter your email">
+         <input type="submit" name="submit" value="Submit">
+     </form>
 
-</html>
-<?php
-if (isset($_POST['login'])) {
+ </body>
 
-    if (!empty($_POST['username']) && !empty($_POST['password'])) {
-        $_SESSION['username'] = $_POST['username'];
-        $_SESSION['password'] = $_POST['password'];
-        header("location:home.php");
-    } else {
-        echo "please fill in the fields";
+ </html>
+
+ <?php
+
+    if (isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+
+
+
+        echo "Name: " . $name . "<br>";
+        echo "Email: " . $email;
+
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $_SESSION['name'] = $name;
+            $_SESSION['email'] = $email;
+        }
+
+        session_start();
+        header("Location: home.php");
     }
-}
 
-?>
+
+
+
+    ?>
